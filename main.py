e21e23067939ae0110c55a2fa7014e9b900043be
gracie2 <stevenmanangu360@gmail.com>
2022-03-21 18:27:14 +0300

    Tested the 7 models to see their performance

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from deepface import DeepFace 
+import matplotlib.pyplot as plt
+import cv2
 
-img1_path = 'images/yara1.jpg'
-img2_path = 'images/luca.jpg'
 
-model_name = 'Facenet'
+img1_path = 'images/luca.jpg'
+img2_path = 'images/luca2.jpg'
+
+models = ['VGG-Face', 'Facenet', 'OpenFace', 'DeepFace', 'DeepID', 'Dlib', 'ArcFace', 'Ensemble']  #Ensemble is a combination of all the models, its super accurate but slow AF...not recommended
+model_name = models[0]
 
 resp = DeepFace.verify(img1_path = img1_path, img2_path = img2_path, model_name = model_name)
 
@@ -14,3 +18,16 @@ if resp['verified'] == True:
     
 else:
     print('Different person')
+    
+#see the face detection in action
+
+img1 = DeepFace.detectFace(img1_path)
+img2 = DeepFace.detectFace(img2_path)
+
+img1 = cv2.cvtColor(img1, cv2.COLOR_RGB2BGR)
+img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2BGR)
+
+cv2.imshow("Image", img1)
+cv2.imshow("Image2", img2)
+
+cv2.waitKey(0)
