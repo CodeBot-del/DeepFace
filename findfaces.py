b2dd210ed862ca88bb1c7c098b7001c81e7e8b12
gracie2 <stevenmanangu360@gmail.com>
2022-03-22 13:11:05 +0300

    performed facial analysis

--- findfaces.py
+++ findfaces.py
@@ -6,8 +6,14 @@ import pandas as pd
 #find function returns whether the face is found or not found. 
 #it takes two basic arguments... the image to be compared, and the path to the database, other arguments like the model, face detector, and so on can also be defined
 
-image = 'luka2.jpeg'
+image = 'imagetest/luka2.jpeg'
 #store results in a dataframe, to see the results of all image comparings
 df = DeepFace.find(img_path = image, db_path = 'images')
 
-print(df.head())
+# print(df.head())
+
+#Analyze the face (you can pass actions to specify analysis, actions = ['emotion', 'age', 'gender', 'race') #without specifying, the function returns both actions.
+
+obj = DeepFace.analyze(img_path= image)
+print(obj)             
+
